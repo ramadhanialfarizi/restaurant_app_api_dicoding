bb79d54df1aa5f2237f16b729eee8b949c19dd40
ramadhanialfarizi <72063724+ramadhanialfarizi@users.noreply.github.com>
2023-02-21 01:22:12 +0700

    fix consumer provider

--- lib/app/view/home_pages/home_pages.dart
+++ lib/app/view/home_pages/home_pages.dart
@@ -50,87 +50,111 @@ class _HomePagesState extends State<HomePages> {
                 const SizedBox(
                   height: 20,
                 ),
-                SearchButton(),
+                const SearchButton(),
                 const SizedBox(
                   height: 40,
                 ),
                 Consumer<HomeProvider>(
-                  builder: (_, restaurant, __) {
-                    return ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: restaurant.resto.length,
-                      itemBuilder: (context, index) {
-                        var initial = restaurant.resto[index];
-                        return InkWell(
-                          onTap: () {},
-                          child: Card(
-                            elevation: 0,
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    child: Image.network(
-                                      'https://restaurant-api.dicoding.dev/images/small/${initial.pictureId}',
-                                      scale: 3.9,
+                  builder: (context, restaurant, __) {
+                    if (restaurant.state == ResultState.loading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (restaurant.state == ResultState.hasData) {
+                      return ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: restaurant.restoList.restaurants.length,
+                        itemBuilder: (context, index) {
+                          var initial = restaurant.restoList.restaurants[index];
+                          return InkWell(
+                            onTap: () {},
+                            child: Card(
+                              elevation: 0,
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      child: Image.network(
+                                        'https://restaurant-api.dicoding.dev/images/small/${initial.pictureId}',
+                                        scale: 3.9,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          '${initial.name}',
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            initial.name.toString(),
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
                                           ),
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Row(
-                                          children: [
-                                            const Icon(
-                                              Icons.location_pin,
-                                              color: Colors.blue,
-                                            ),
-                                            const SizedBox(
-                                              width: 5,
-                                            ),
-                                            Text('${initial.city}'),
-                                          ],
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Row(
-                                          children: [
-                                            const Icon(Icons.star,
-                                                color: Colors.amber),
-                                            const SizedBox(
-                                              width: 5,
-                                            ),
-                                            Text(
-                                              '${initial.rating}',
-                                            ),
-                                          ],
-                                        )
-                                      ],
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          Row(
+                                            children: [
+                                              const Icon(
+                                                Icons.location_pin,
+                                                color: Colors.blue,
+                                              ),
+                                              const SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(initial.city.toString()),
+                                            ],
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          Row(
+                                            children: [
+                                              const Icon(Icons.star,
+                                                  color: Colors.amber),
+                                              const SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(
+                                                '${initial.rating}',
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    );
+                          );
+                        },
+                      );
+                    } else if (restaurant.state == ResultState.noData) {
+                      return Center(
+                        child: Material(
+                          child: Text(restaurant.message),
+                        ),
+                      );
+                    } else if (restaurant.state == ResultState.error) {
+                      return Center(
+                        child: Material(
+                          child: Text(restaurant.message),
+                        ),
+                      );
+                    } else {
+                      return Center(
+                        child: Material(
+                          child: Text(''),
+                        ),
+                      );
+                    }
                   },
                 )
               ],
--- lib/app/view/home_pages/view_model/home_provider.dart
+++ lib/app/view/home_pages/view_model/home_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:restaurant_app_api_dicoding/app/model/data_source/remote_data_source.dart';
 import 'package:restaurant_app_api_dicoding/app/model/restaurant_list_model.dart';
 
-//enum ResultState { loading, noData, hasData, error }
+enum ResultState { loading, noData, hasData, error }
 
 class HomeProvider extends ChangeNotifier {
   final RemoteDataSource remoteDataSource = RemoteDataSource();
@@ -11,22 +11,33 @@ class HomeProvider extends ChangeNotifier {
   //   getAllRestaurantList();
   // }
 
-  List<Restaurant> resto = [];
+  //List<Restaurant> resto = [];
+  late RestaurantList _restaurantList;
+  late ResultState _state;
+  String _message = '';
 
-  RestaurantList? _restaurantList;
-  //late ResultState _state;
+  String get message => _message;
+  RestaurantList get restoList => _restaurantList;
+  ResultState get state => _state;
 
-  RestaurantList? get restoList => _restaurantList;
-  //ResultState get state => _state;
-
-  Future<void> getAllRestaurantList() async {
+  Future<dynamic> getAllRestaurantList() async {
     try {
-      final source = await remoteDataSource.getRestaurantList();
-      resto = source.restaurants.toList();
+      _state = ResultState.loading;
       notifyListeners();
-      //return _restaurantList = source;
+      final source = await remoteDataSource.getRestaurantList();
+      if (source.restaurants.isEmpty) {
+        _state = ResultState.noData;
+        notifyListeners();
+        return _message = 'Empty Data';
+      } else {
+        _state = ResultState.hasData;
+        notifyListeners();
+        return _restaurantList = source;
+      }
     } catch (e) {
-      rethrow;
+      _state = ResultState.error;
+      notifyListeners();
+      return _message = 'Error --> $e';
     }
   }
 }
